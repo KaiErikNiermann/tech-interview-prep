e32ca5f72169d8ab597c2abc9895bac80dbafdb1
KaiErikNiermann <kai.niermann@gmail.com>
2024-04-04 09:43:00 +0200

    added gh source link

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,9 @@
 
 import os 
 import sys
+import inspect
 import commonmark
+import sphinx
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -29,6 +31,35 @@ autodoc_default_options = {
     'exclude-members': '__weakref__'
 }
 
+def linkcode_resolve(domain, info):
+    """
+    Determine the URL given the domain (e.g., 'py') and info dictionary.
+    """
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+
+    # Adjust the following variables to match your project's structure and GitHub repo
+    github_user = 'KaiErikNiermann'
+    github_repo = 'tech-interview-prep'
+    github_branch = 'main'  # or the name of your default branch
+
+    # Getting the module or function's source file and line numbers
+    try:
+        obj = sphinx.util.inspect.safe_getattr(sys.modules[info['module']], info['fullname'])
+        source_file = inspect.getsourcefile(obj)
+        source_file = os.path.relpath(source_file, start=os.path.dirname(os.path.abspath('.')))
+        _, line = inspect.getsourcelines(obj)
+    except Exception:
+        source_file = None
+        line = None
+
+    if source_file and line:
+        return f"https://github.com/{github_user}/{github_repo}/blob/{github_branch}/{source_file}#L{line}"
+    return None
+
+
 # renders markdown in docstrings
 def docstring(app, what, name, obj, options, lines):
     md  = '\n'.join(lines)
@@ -46,6 +77,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'sphinx.ext.linkcode',
     'myst_parser', 
     'sphinx.ext.mathjax', 
     'sphinx_math_dollar'
